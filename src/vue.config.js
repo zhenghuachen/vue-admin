7ebcba58c0dfe733fdc57209ef39de022d46c308
chenzh12 <chenzh12@midea.com>
2024-09-10 17:36:44 +0800

    chore: 增加vue.config.js,更改Vue和VueX的版本

---
+++ src/vue.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  // 配置svg-sprite-loader
+  chainWebpack(config) {
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+  }
+}
