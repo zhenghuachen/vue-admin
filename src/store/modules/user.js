75bdc513f3059b942317b5e6269cc468ea871d6d
zhenghuachen <zhchenanhui@163.com>
2024-09-29 22:43:42 +0800

    feat(login): 封装请求动作

---
+++ src/store/modules/user.js
@@ -0,0 +1,28 @@
+import { login } from '@/api/sys'
+import md5 from 'md5'
+
+export default {
+  namespaced: true, // 独立的模块，防止命名冲突
+  state: () => ({}),
+  mutations: {},
+  actions: {
+    /**
+         * 登录请求动作
+         * @param {*} context
+         * @param {*} userInfo
+         * @returns
+         */
+    login(context, userInfo) {
+      const { username, password } = userInfo
+      return new Promise((resolve, reject) => {
+        login({
+          username, password: md5(password)
+        }).then(data => {
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    }
+  }
+}
